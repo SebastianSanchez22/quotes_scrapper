51c9b080cdd62da71136f28a10259db438a16ca4
Sebastian Sanchez <sebastiansaor22@gmail.com>
2023-01-11 23:16:46 -0500

    Requesting top n tags instead of all tags

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -3,6 +3,7 @@ import scrapy
 # Titulo = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+# Next page button = //li[@class="next"]/a/@href
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -11,32 +12,41 @@ class QuotesSpider(scrapy.Spider):
     ]
     custom_settings = {
         'FEED_URI':'quotes.json',
-        'FEED_FORMAT':'json',    
+        'FEED_FORMAT':'json',
+        'FEED_EXPORT_ENCODING': 'utf-8' ,
+        'CONCURRENT_REQUESTS': 24,
+        'MEMUSAGE_LIMIT_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': ['sebastiansaor22@gmail.com'],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'SebastianSanchez'
     }
 
     def parse_only_quotes(self, response, **kwargs):
         if kwargs:
-            quotes = kwargs['quotes']
-        quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall())
+            kwargs['quotes'].extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall())
 
         next_page_button_link = response.xpath('//li[@class="next"]/a/@href').get()
     
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs=kwargs)
         else:
-            yield {
-                'quotes': quotes
-            }
+            yield kwargs
 
 
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        top_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+
+        top = getattr(self, 'top', None)
+        if top:
+            top = int(top)
+            top_tags = top_tags[:top]
+
 
         yield {
             'title': title, 
-            'top_ten_tags': top_ten_tags
+            'top_tags': top_tags
         }
 
         next_page_button_link = response.xpath('//li[@class="next"]/a/@href').get()
