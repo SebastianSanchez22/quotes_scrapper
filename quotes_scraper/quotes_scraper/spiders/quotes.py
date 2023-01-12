6d3246f44ddbdbcf16c34ff9d00a79021531de97
Sebastian Sanchez <sebastiansaor22@gmail.com>
2023-01-11 23:00:34 -0500

    Scrapping multiple quotes

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -9,6 +9,25 @@ class QuotesSpider(scrapy.Spider):
     start_urls = [
         'http://quotes.toscrape.com'
     ]
+    custom_settings = {
+        'FEED_URI':'quotes.json',
+        'FEED_FORMAT':'json',    
+    }
+
+    def parse_only_quotes(self, response, **kwargs):
+        if kwargs:
+            quotes = kwargs['quotes']
+        quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall())
+
+        next_page_button_link = response.xpath('//li[@class="next"]/a/@href').get()
+    
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+        else:
+            yield {
+                'quotes': quotes
+            }
+
 
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
@@ -17,6 +36,17 @@ class QuotesSpider(scrapy.Spider):
 
         yield {
             'title': title, 
-            'quotes': quotes,
             'top_ten_tags': top_ten_tags
         }
+
+        next_page_button_link = response.xpath('//li[@class="next"]/a/@href').get()
+        # Importante preguntar si la variable next_page_button_link contiene algo 
+				# ya que por logica en algun momento llegariamos a ultima pagina.
+        if next_page_button_link:
+            # El metodo follow nos permite seguir al link (lo que hace scrapy es 
+						# dejar la url absoluta y cambiar el resto) 
+            # Este metodo posee un callback es decir un metodo que se ejecutara 
+						# automaticamente despues de haber cambiado de url.
+            yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+
+   
