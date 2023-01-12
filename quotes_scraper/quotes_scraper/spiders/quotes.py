ceac57ef8acdf037f7cc4966e897cb089d9112de
Sebastian Sanchez <sebastiansaor22@gmail.com>
2023-01-11 22:02:38 -0500

    Quotes scrapper 1st version (title, quotes, top_ten_tags)

---
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -0,0 +1,22 @@
+import scrapy
+
+# Titulo = //h1/a/text()
+# Citas = //span[@class="text" and @itemprop="text"]/text()
+# Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'http://quotes.toscrape.com'
+    ]
+
+    def parse(self, response):
+        title = response.xpath('//h1/a/text()').get()
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+
+        yield {
+            'title': title, 
+            'quotes': quotes,
+            'top_ten_tags': top_ten_tags
+        }
